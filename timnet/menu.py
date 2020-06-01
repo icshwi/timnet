7af84d716610d5d688f77b7ad5dcf71006699e87
Jerzy Jamroz <jerzy.jamroz@gmail.com>
2020-06-01 12:04:23 +0200

    feat: Each inventory file (can be many) allows a custom link. TD moved to a proper file.

--- timnet/menu.py
+++ timnet/menu.py
@@ -8,14 +8,22 @@ import argparse
 def menu():
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--verbosity", action="count", default=0)
-    parser.add_argument("-n", "--net", action='store_true', help="Read the current network topology via Epics")
-    parser.add_argument("-p", "--plot", action='store_true', help="Plot the actual network topology")
-    parser.add_argument("-d", "--dev", action='store_true', help="Apply devel settings - *_dev files")
-    parser.add_argument("-c", "--check", action='store_true', help="Checks the timing network anomalies")
-    parser.add_argument("-m", "--manual", action='store_true', help="Print the application manual")
+    parser.add_argument("-n", "--net", action='store_true', help="Read the current network topology via Epics.")
+    parser.add_argument("-p", "--plot", action='store_true', help="Plot the actual network topology.")
+    parser.add_argument("-d", "--dev", action='store_true', help="Apply devel settings - *_dev files.")
+    parser.add_argument("-c", "--check", action='store_true', help="Checks the timing network anomalies.")
+    parser.add_argument("-m", "--manual", action='store_true', help="Print the application manual.")
+    parser.add_argument("-f", "--inventory", action='store', default="json/example.json", help="Network inventory with the PV prefixes.")
     args = parser.parse_args()
     if args.verbosity >= 2:
         print("Running '{}'".format(__file__))
     if args.verbosity >= 1:
         print(args)
-    return {"net": args.net, "plot": args.plot, "dev": args.dev, "manual": args.manual, "check": args.check}
+    #return {"net": args.net, "plot": args.plot, "dev": args.dev, "manual": args.manual, "check": args.check}
+    return args
+
+
+if __name__ == '__main__':
+    args = menu()
+    print(args)
+    print(args.check)
--- timnet/timnet.py
+++ timnet/timnet.py
@@ -21,30 +21,32 @@ def main():
     # Check epics
     logging.info("epics " + str(epics.ca.find_libca()))
 
-    # Interface
-    interface = {"dir": "json/", "inventory": "inventory.json", "network": "network.json", "plot": "plot.json", "limits": "limits.json"}
-
     # Get the cli params
     cli_params = menu()
-    if cli_params["manual"]:
+    if cli_params.manual:
         print(open('README.md').read())
-    param_net = cli_params["net"]
-    param_plot = cli_params["plot"]
-    param_check = cli_params["check"]
-    # declare meta list to log the program progress
 
-    if cli_params["dev"] is True:
+    # Interface
+    interface = {"inventory": cli_params.inventory,
+                 "network": cli_params.inventory.replace(".json", "_net.json"),
+                 "plot": cli_params.inventory.replace(".json", "_plot.json"),
+                 "limits": "json/limits.json",
+                 "errors": cli_params.inventory.replace(".json", "_err.json")
+                 }
+
+    if cli_params.dev:
         for el in interface:
             interface[el] = interface[el].replace(".json", "_dev.json")
 
+    # declare meta list to log the program progress
     logging.info("cli_params " + str(cli_params))
     logging.info("interface " + str(interface))
-    if param_net:
-        tnet.main(inventory_jl=interface["dir"] + interface["inventory"], network_jl=interface["dir"] + interface["network"])
-    if param_plot:
-        tplot.main(network_jl=interface["dir"] + interface["network"], plot_jl=interface["dir"] + interface["plot"])
-    if param_check:
-        tcheck.main(network_jl=interface["dir"] + interface["network"], limits_jl=interface["dir"] + interface["limits"])
+    if cli_params.net:
+        tnet.main(inventory_jl=interface["inventory"], network_jl=interface["network"])
+    if cli_params.plot:
+        tplot.main(network_jl=interface["network"], plot_jl=interface["plot"])
+    if cli_params.check:
+        tcheck.main(network_jl=interface["network"], limits_jl=interface["limits"], output_jl=interface["errors"])
 
 
 if __name__ == '__main__':
