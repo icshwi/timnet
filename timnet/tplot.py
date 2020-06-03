67a2c64487b6bfda5ea33af036591057c3290359
Jerzy Jamroz <jerzy.jamroz@gmail.com>
2020-06-03 12:51:07 +0200

    feat: tplot adjusted for the new inventory.

--- timnet/tplot.py
+++ timnet/tplot.py
@@ -33,28 +33,28 @@ net_plot = {}
 net_plot_nc = {}
 
 
-def net_node_add(ioc_str, tim_net_json, level):
-    # print(tim_net_json[ioc_str]["ID"][-2])
-    _id_int = int(tim_net_json[ioc_str]["ID"])
-    id_tmp = tim_net_json[ioc_str]["ID"][::-1]
+def net_node_add(prefix_str, tim_net_json, level):
+    # print(tim_net_json[prefix_str]["ID"][-2])
+    _id_int = int(tim_net_json[prefix_str]["ID"])
+    _id_hex_str = tim_net_json[prefix_str]["ID"][::-1]
 
-    if "EVR" in ioc_str:
-        dev_tmp = evr_const.copy()
+    if "EVR" in prefix_str:
+        _dev = evr_const.copy()
     else:
-        dev_tmp = evm_const.copy()
-    dev_tmp["dev"] = ioc_str
-    dev_tmp["id"] = hex2short(tim_net_json[ioc_str]["ID"])
+        _dev = evm_const.copy()
+    _dev["dev"] = prefix_str
+    _dev["id"] = hex2short(tim_net_json[prefix_str]["ID"])
 
     if _id_int < 0:
-        net_plot_nc[dev_tmp["dev"]] = dev_tmp.copy()
+        net_plot_nc[_dev["dev"]] = _dev.copy()
         return -1
 
-    if int(id_tmp[level]) != 0 and int(id_tmp[level+1]) == 0:
-        if net_plot[int(id_tmp[level])] == {}:
-            net_plot[int(id_tmp[level])] = dev_tmp.copy()
+    if int(_id_hex_str[level]) != 0 and int(_id_hex_str[level+1]) == 0:
+        if net_plot[int(_id_hex_str[level])] == {}:
+            net_plot[int(_id_hex_str[level])] = _dev.copy()
         else:
-            net_plot[int(id_tmp[level])][int(id_tmp[level-1])] = dev_tmp.copy()
-        dev_tmp.clear()
+            net_plot[int(_id_hex_str[level])][int(_id_hex_str[level-1])] = _dev.copy()
+        _dev.clear()
 
 
 def del_none(d):
@@ -73,15 +73,18 @@ def main(network_jl, plot_jl):
     with open(network_jl) as infile:
         tim_net_json = json.load(infile)
 
-    for ioc_str in tim_net_json:
-        tim_net_json[ioc_str]["ID"] = format(tim_net_json[ioc_str]["ID"], "09x")
+    for prefix_str in tim_net_json:
+        for pvi_str in tim_net_json[prefix_str]:
+            if "-ID-I" in pvi_str:
+                tim_net_json[prefix_str]["ID"] = format(tim_net_json[prefix_str][pvi_str], "09x")
+                break
 
     # Find EVG
-    for ioc_str in tim_net_json:
-        if int(tim_net_json[ioc_str]["ID"]) == 0:
+    for prefix_str in tim_net_json:
+        if int(tim_net_json[prefix_str]["ID"]) == 0:
             evm_tmp = evm_const
-            evm_tmp["dev"] = ioc_str
-            evm_tmp["id"] = hex2short(tim_net_json[ioc_str]["ID"])
+            evm_tmp["dev"] = prefix_str
+            evm_tmp["id"] = hex2short(tim_net_json[prefix_str]["ID"])
             net_plot.update(evm_tmp)
             break
 
@@ -90,8 +93,8 @@ def main(network_jl, plot_jl):
 
     # Build the network plot
     for lv in range(0, 8):
-        for ioc_str in tim_net_json:
-            net_node_add(ioc_str, tim_net_json, lv)
+        for prefix_str in tim_net_json:
+            net_node_add(prefix_str, tim_net_json, lv)
 
     with open(plot_jl, 'w') as outfile:
         json.dump({**net_plot, **net_plot_nc}, outfile)
